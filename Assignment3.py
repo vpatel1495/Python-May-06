b0af1cc6b17244e1ae5484fd53fa2807450f0b94
Vishal Patel <35545470+vpatel1495@users.noreply.github.com>
2019-06-05 14:58:08 -0400

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,9 +1,9 @@
 #Assignement 3
 #PROBLEM 1
-    #Create a program to do the following tasks. Note: Without using inbuild functions like sort, min, sorted, reverse, split
-        #A) Split a given sentence into a list of words.
-        #B) Sort that list accordingly to the letters alphabetically.
-        #C) Sort that list alphabetically.
+    # Create a program to do the following tasks. Note: Without using inbuild functions like sort, min, sorted, reverse, split
+        # A) Split a given sentence into a list of words.
+        # B) Sort that list accordingly to the letters alphabetically.
+        # C) Sort that list alphabetically.
 
 sentence =input("Type any sentence, end sentence with a space: ")
 listA=[]
@@ -24,14 +24,17 @@ listB = listA[:]
 def assort_sent(self):
     n=len(self)
     x=0
-    for i in range(0, n+1):
-        x=0
-        while x< (n-1):
-            if ord(self[x][0])> ord(self[x+1][0]):
-                listB.append(listB[x])
-                del(listB[x])
-            else:
-                x=x+1
+    for i in range(0, n):
+        m=len(self[i])
+        for j in range (0, m):
+            while x< (n-1):
+                if ord(self[x][0])>ord(self[x+1][0]):
+                    listB.append(listB[x])
+                    del(listB[x])
+                    x=x+1
+                else:
+                    x=x+1
+            x=0
     print(listB)
 
 assort_sent(listB)
@@ -56,18 +59,22 @@ def list_word(self):
 list_word(listC)
 
 list2=list1[:]
-def assort_word(self):
+def assrt(self):
+    def assort_word(self):
+        n = len(self)
+        for i in range(0, n):
+            m = len(self[i])
+            for j in range(0, m-1):
+                if ord(self[i][j]) > ord(self[i][j + 1]):
+                    list2[i].append(self[i][j])
+                    del list2[i][j]
     n=len(self)
-    for i in range(0, n-1):
-        m=len(self[i])
-        for j in range (0, m-1):
-            if ord(self[i][j])>ord(self[i][j+1]):
-                list2[i].append(self[i][j])
-                del list2[i][j]
-        list2.append(list1)
+    for i in range(0, n):
+        m =len(self[i])
+        for j in range(0, m):
+            assort_word(self)
     print(list2)
 
-
-assort_word(list1)
+assrt(list1)
 
 #This will sort the list of words alphabetically
